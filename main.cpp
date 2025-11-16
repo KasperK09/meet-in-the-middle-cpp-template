795c70433eefe07ee69f8ff4651c9fdc1368de25
Kasper <kamkasper@gmail.com>
2025-11-16 12:45:10 -0600

    organize

--- main.cpp
+++ main.cpp
@@ -11,35 +11,32 @@ int main()
 
     ifstream fin(filename);
     if (!fin.is_open()) {
-        cerr << "Error: Could not open file.\n";
+        cerr << "File could not be opened\n";
         return 1;
     }
 
-    // ---- First pass: count how many integers are in the CSV ----
     int count = 0;
     int temp;
     char comma;
 
     while (fin >> temp) {
         count++;
-        fin >> comma;  // Try to read a comma (fails on last entry, but that's ok)
+        fin >> comma;
     }
 
     if (count == 0) {
-        cerr << "Error: File is empty.\n";
+        cerr << "File is empty.\n";
         return 1;
     }
 
-    // ---- Allocate dynamic array ----
     int* data = new(nothrow) int[count];
     if (data == nullptr) {
-        cerr << "Error: Memory allocation failed.\n";
+        cerr << "Memory allocation failed.\n";
         return 1;
     }
 
-    // ---- Second pass: load data ----
-    fin.clear();        // Reset stream state
-    fin.seekg(0);       // Rewind to file beginning
+    fin.clear();
+    fin.seekg(0);
 
     int index = 0;
     while (fin >> temp) {
@@ -49,23 +46,18 @@ int main()
 
     fin.close();
 
-    // ---- Compute the median ----
     double median = 0.0;
 
     if (count % 2 == 1) {
-        // Odd number of elements
         median = data[count / 2];
     } else {
-        // Even: average of two middle elements
         int mid1 = data[(count / 2) - 1];
         int mid2 = data[count / 2];
         median = (mid1 + mid2) / 2.0;
     }
 
-    // ---- Output result ----
     cout << "The median of the dataset is " << median << endl;
 
-    // ---- Clean up ----
     delete[] data;
 
     return 0;
